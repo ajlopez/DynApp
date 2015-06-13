99f826677b6f81276e8db8056d65ebdebb40470f
ajlopez <webmaster@ajlopez.com>
2015-06-13 08:26:59 -0300

    Create text field

---
+++ test/fields.js
@@ -0,0 +1,10 @@
+
+var Fields = require('../lib/fields').createFields();
+
+exports['Create text field'] = function (test) {
+    var builder = Fields.text('First Name');
+    var field = builder.build();
+    
+    test.ok(field);
+    test.equal(field.title, 'First Name');
+}
