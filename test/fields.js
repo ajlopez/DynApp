7c579ce397c3617369906f36b905c865f911337d
ajlopez <webmaster@ajlopez.com>
2015-06-13 12:27:32 -0300

    Required text field

--- test/fields.js
+++ test/fields.js
@@ -9,3 +9,13 @@ exports['Create text field'] = function (test) {
     test.equal(field.title, 'First Name');
     test.equal(field.type, 'text');
 }
+
+exports['Create required text field'] = function (test) {
+    var builder = Fields.text('First Name').required();
+    var field = builder.build();
+    
+    test.ok(field);
+    test.equal(field.title, 'First Name');
+    test.equal(field.type, 'text');
+    test.ok(field.required);
+}
