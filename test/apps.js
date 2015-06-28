dcf548b1af53712feaedc53bff6e427d9e8c11e0
ajlopez <webmaster@ajlopez.com>
2015-06-28 08:21:14 -0300

    Build relation in reference field

--- test/apps.js
+++ test/apps.js
@@ -70,3 +70,55 @@ exports['build application with two entities'] = function (test) {
     test.equal(app.entities.supplier.fields.created.title, 'Created at');
     test.ok(!app.entities.supplier.fields.created.required);
 };
+
+exports['build application with two related entities'] = function (test) {
+    var app = apps.build({
+        name: 'myapp',
+        title: 'My App',
+        entities: function (Fields) {
+            return {
+                department: {
+                    descriptor: 'Department',
+                    fields: {
+                        name: Fields.text('Name').required()
+                    }
+                },
+                employee: {
+                    descriptor: 'Employee',
+                    fields: {
+                        name: Fields.text('Name').required(),
+                        department: Fields.reference('Department').entity('department')
+                    }
+                }
+            }
+        }
+    });
+    
+    test.ok(app);
+    test.equal(app.name, 'myapp');
+    test.equal(app.title, 'My App');
+    test.ok(app.entities);
+
+    test.ok(app.entities.department);
+    test.equal(app.entities.department.descriptor, 'Department');
+    test.ok(app.entities.department.fields);
+
+    test.ok(app.entities.department.fields.name);
+    test.equal(app.entities.department.fields.name.type, 'text');
+    test.equal(app.entities.department.fields.name.title, 'Name');
+    test.ok(app.entities.department.fields.name.required);
+
+    test.ok(app.entities.employee);
+    test.equal(app.entities.employee.descriptor, 'Employee');
+    test.ok(app.entities.employee.fields);
+
+    test.ok(app.entities.employee.fields.name);
+    test.equal(app.entities.employee.fields.name.type, 'text');
+    test.equal(app.entities.employee.fields.name.title, 'Name');
+    test.ok(app.entities.employee.fields.name.required);
+
+    test.ok(app.entities.employee.fields.department);
+    test.equal(app.entities.employee.fields.department.type, 'reference');
+    test.equal(app.entities.employee.fields.department.title, 'Department');
+    test.strictEqual(app.entities.employee.fields.department.entity, app.entities.department);
+};
